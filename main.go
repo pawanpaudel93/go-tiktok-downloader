cda3a59fa0dbd25718d4c3915565fbbfc56c411a
Pawan Paudel <pawanpaudel93@gmail.com>
2020-11-19 21:10:44 +0545

    doing error handeling

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pawanpaudel93/go-tiktok-downloader/tiktok"
 )
 
 func main() {
 	baseDIR := "./downloads"
 	video := tiktok.Video{URL: "https://www.tiktok.com/@manjuxettri07/video/6886732628280593666", UseProxy: false, BaseDIR: baseDIR}
-	_, _ = video.GetVideoInfo()
-	_ = video.Download()
+	_, err := video.GetVideoInfo()
+	if err != nil {
+		err = video.Download()
+		if err == nil {
+			fmt.Println("Video Downloaded Successfully!!!")
+		} else {
+			fmt.Println("ERROR: ", err)
+		}
+	}
+
 	profile := tiktok.Profile{URL: "https://www.tiktok.com/@manjuxettri07", UseProxy: false, BaseDIR: baseDIR}
-	_, _ = profile.GetProfileInfo()
-	profile.DownloadPhoto("large")
+	_, err = profile.GetProfileInfo()
+	if err != nil {
+		err = profile.DownloadPhoto("large")
+		if err == nil {
+			fmt.Println("Profile Image Downloaded Successfully!!!")
+		} else {
+			fmt.Println("ERROR: ", err)
+		}
+	}
 }
